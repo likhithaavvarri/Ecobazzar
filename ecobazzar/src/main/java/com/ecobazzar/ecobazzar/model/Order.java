0cebcc60244462f38dc74d7ab08b38559831c56b
likhithaavvari <avvarilikhitha28@gmail.com>
2025-10-20 10:59:33 +0530

    2nd commit

--- ecobazzar/src/main/java/com/ecobazzar/ecobazzar/model/Order.java
+++ ecobazzar/src/main/java/com/ecobazzar/ecobazzar/model/Order.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//order table
 @Entity
 @Table(name = "orders")
 public class Order {
